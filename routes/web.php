3137c5c70fec4192b30485873576e8e9324d91e6
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-11-14 08:37:13 -0300

    initial structure for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// Login Routes
+Route::get('/login', [AuthController::class, 'index'])->name('login.index');
